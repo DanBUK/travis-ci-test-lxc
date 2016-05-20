d360a31598474a51fef5c08d074795b1dd2b4a1c
DanBUK <dan@highops.com>
2016-05-20 20:17:08 +0100

    make test actually work

--- tests/test1.py
+++ tests/test1.py
@@ -3,14 +3,9 @@ import unittest
 def fun(x):
   return x + 1
 
-def badfun(x):
-  return x
-
-
 class MyTest(unittest.TestCase):
   def test(self):
     self.assertEqual(fun(3), 4)
-#     self.assertEqual(badfun(3), 4)
 
 if __name__ == '__main__':
   unittest.main()
